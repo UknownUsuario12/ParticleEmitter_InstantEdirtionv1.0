c84509bb1ff10c7759285ea600be466c1c8acc21
UknownUsuario12 <147109010+UknownUsuario12@users.noreply.github.com>
2023-10-05 18:11:03 -0500

    Update run.lua

--- run.lua
+++ run.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UknownUsuario12/ParticleEmitter_InstantEdirtionv1.0/main/Part1.lua"))()
+task.wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UknownUsuario12/ParticleEmitter_InstantEdirtionv1.0/main/Part2.lua"))()
+task.wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UknownUsuario12/ParticleEmitter_InstantEdirtionv1.0/main/Main.lua"))()
