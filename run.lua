80052aeba06939853e0d3c16e4fc87bc77a111d8
UknownUsuario12 <147109010+UknownUsuario12@users.noreply.github.com>
2023-10-05 17:18:49 -0500

    Create run.lua

---
+++ run.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UknownUsuario12/ParticleEmitter_InstantEdirtionv1.0/main/Part1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UknownUsuario12/ParticleEmitter_InstantEdirtionv1.0/main/Part1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UknownUsuario12/ParticleEmitter_InstantEdirtionv1.0/main/Main.lua"))()
